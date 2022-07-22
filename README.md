9051ea32466ab124e59fd52c09a767e6932a2b17
Lolicanadian <50314143+Lolicanadian@users.noreply.github.com>
2022-07-21 20:19:02 -0600

    emailjs integrated into contacts section.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 My personal self-portfolio that is built using React JS for the front-end interface. 
 
+//------ IMPORTANT INSTALLATION TOOLS -------------------//
 Install the 'swiper' library to ensure the swipers work. 
 ### `npm install swiper`
+
+I had to create an emailjs account in order to set up my contact information page where users can send me a direct message through the website.
+
+Install the 'emailjs' library to ensure that the email works.
+### `npm install emailjs-com --save`
 
 //------------------------- HOW TO RUN -----------------------------------//
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,19 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_o0fg2vc', 'template_rs20yuo', form.current, 'WHVdy-jYRA2bQ1j2t')
+       
+        e.target.reset()
+      };
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -37,7 +48,7 @@ const Contact = () => {
                 {/* END OF CONTACT OPTIONS */}
 
                 {/* Form where people are required to enter a name.*/}
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Your Full Name' required />
                     <input type="email" name='email' placeholder='Your Email' required />
                     <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
